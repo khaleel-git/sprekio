119805bf5142876ea6f6d03983aa9dfdbd7c992e
Khaleel Ahmad <khaleel.org@gmail.com>
2026-09-09 17:17:38 +0200

    Revert "chore: disable word highlighter"
    This reverts commit a333e97316ceb49245bd3a408c7924d705155190.

--- Chrome Extension/src/content/index.tsx
+++ Chrome Extension/src/content/index.tsx
@@ -1147,7 +1147,7 @@ const SprekioOverlay: React.FC = () => {
       }
       wordCounter++;
       const isSaved = savedWordsSet.has(token.toLowerCase());
-      const isSpeaking = false; // wordCounter === activeWordIndex; // Disabled word highlighter for now
+      const isSpeaking = wordCounter === activeWordIndex;
       const idleBg = isSpeaking ? '#f97316' : (isSaved ? 'rgba(249,115,22,0.22)' : 'transparent');
       const idleColor = isSpeaking ? '#ffffff' : 'inherit';
       const pos = grammarColors ? VocabularyEngine.getPartOfSpeechSync(token) : undefined;
